d86e44b752b4ef2a96c7910c68837bd112707921
Andy Warns <awarns@thirtyonegifts.com>
2013-11-14 15:15:10 -0500

    CA Party

--- features/step_definitions/employee_party_ca_steps.rb
+++ features/step_definitions/employee_party_ca_steps.rb
@@ -1,6 +1,6 @@
-When /^I validate party items as an employee$/ do
+When /^I validate party items as an employee in Canada$/ do
 
-  @server = "https://uatpps.toguat.local"
+  @server = "https://uatleaf.toguat.local"
 
 
   @count = 1
@@ -15,9 +15,9 @@ When /^I validate party items as an employee$/ do
     @browser.link(:id, "ContentPlaceHolder1_btnLogin").click
     @browser.goto("#{@server}/employee/admin/frm_orderchoose.aspx")
 
-    @browser.goto("#{@server}/employee/admin/frm_orderchoose.aspx?OT=PTY")
+    @browser.goto("#{@server}/employee/admin/frm_orderchoose.aspx?OT=CPT")
 
-    @browser.text_field(:id, "ContentPlaceHolder1_idnobox").set("102091")
+    @browser.text_field(:id, "ContentPlaceHolder1_idnobox").set("231211")
     @browser.button(:value, "Enter Order").click
 
 
@@ -29,9 +29,10 @@ When /^I validate party items as an employee$/ do
     @browser.text_field(:id, "ctl00_ContentPlaceHolder1_event_date_dateInput").set("6/6/2013")
     @browser.text_field(:id, "ContentPlaceHolder1_ship_fname").set("Andy")
     @browser.text_field(:id, "ContentPlaceHolder1_ship_lname").set("Warns")
-    @browser.text_field(:id, "ContentPlaceHolder1_shipping_info_txt_street_1").set("205 N. Nelson Rd.")
-    @browser.text_field(:id, "ContentPlaceHolder1_shipping_info_txtPostalCode").set("43219")
-
+    @browser.text_field(:id, "ContentPlaceHolder1_shipping_info_txt_street_1").set("205 N. Nelson Rd")
+    sleep(2)
+    @browser.text_field(:id, "ContentPlaceHolder1_shipping_info_txt_city").set("Columbus")
+    @browser.text_field(:id, "ContentPlaceHolder1_shipping_info_txtPostalCode").set("N8N 1A1")
     while @browser.link(:id, "ContentPlaceHolder1_btn_save").exists?
 
       @browser.link(:id, "ContentPlaceHolder1_btn_save").click
@@ -60,21 +61,29 @@ When /^I validate party items as an employee$/ do
   end
 
 
+
+
   while @rows < 1200
 
     if @count == 2
 
 
-
+      product_price = @wrksheet.Cells(@rows, "AN").text
       @product_desc = @wrksheet.Cells(@rows, "G").text
 
+      while product_price == "#N/A"
 
-      #while @product_desc =~ /.*Collegiate.*/
-      #
-      #  @rows = @rows + 1
-      #  @product_desc = @wrksheet.Cells(@rows, "G").text
-      #
-      #end
+        @rows = @rows + 1
+        product_price = @wrksheet.Cells(@rows, "AN").text
+
+      end
+
+      while @product_desc =~ /.*Collegiate.*/
+
+        @rows = @rows + 1
+        @product_desc = @wrksheet.Cells(@rows, "G").text
+
+      end
 
       @productid = @wrksheet.Cells(@rows, "F").value
 
@@ -90,7 +99,7 @@ When /^I validate party items as an employee$/ do
       if @browser.text.include?('That item or description that you entered could not be found in our database and cannot be added to your order') or @browser.text.include?('The item that you entered is not allowed for this type of order')
 
 
-        puts "SKU #{@productid} Could not be added to a party order through employee back office in the US."
+        puts "SKU #{@productid} Could not be added to a party order through employee back office in Canada."
 
         @count = 2
         @rows = @rows + 1
@@ -111,6 +120,8 @@ When /^I validate party items as an employee$/ do
 
 
   end
+
+
 
 
 
